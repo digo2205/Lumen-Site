2a89f8791efbcaacdf6cae05e3e5c7b9b585291e
Rodrigo Montechezi <digo2205>
2026-06-30 13:44:57 -0300

    update

--- src/pages/DownloadPage.tsx
+++ src/pages/DownloadPage.tsx
@@ -3,12 +3,12 @@ import { useEffect } from "react";
 export function DownloadModpackPage() {
   useEffect(() => {
     // URL direta do Dropbox (com ?dl=1)
-    const fileUrl = "https://www.dropbox.com/";
+    const fileUrl = "https://www.dropbox.com/scl/fi/dtv27mhujcauv4b3z4326/Lumen-v1.0.zip?rlkey=yr47eeej170zlof0d7c9zgjbk&st=glftc25n&dl=1";
 
     // Cria um link temporário e dispara o download
     const link = document.createElement("a");
     link.href = fileUrl;
-    link.download = "lumen-modpack-1.0.zip";
+    link.download = "Lumen-v1.0.zip";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -22,7 +22,7 @@ export function DownloadModpackPage() {
       <p className="text-muted-foreground">
         Se o download não começar automaticamente,{" "}
         <a
-          href="https://www.dropbox.com/"
+          href="https://www.dropbox.com/scl/fi/dtv27mhujcauv4b3z4326/Lumen-v1.0.zip?rlkey=yr47eeej170zlof0d7c9zgjbk&st=glftc25n&dl=1"
           className="text-primary hover:underline font-medium"
         >
           clique aqui
