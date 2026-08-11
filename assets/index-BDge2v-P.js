97ae47049d23f41cf68f07d0c84bf75cab1b7357
Rodrigo Montechezi <digo2205>
2026-08-11 00:02:35 -0300

    Updates

--- assets/index-BDge2v-P.js
+++ assets/index-BDge2v-P.js
@@ -23512,7 +23512,7 @@ function DefinicoesPage() {
 
 function DownloadModpackPage() {
   reactExports.useEffect(() => {
-    const fileUrl = "https://www.dropbox.com/scl/fi/5fyyc5ut4uki0in5n42de/Lumen-v1.5.zip?rlkey=e7cdjysnz3dpsegctsobbuahu&st=xfy64z7q&dl=1";
+    const fileUrl = "https://www.dropbox.com/scl/fi/6jxupkt4jl0jxqub2m7gp/Lumen-2.2.zip?rlkey=10afp3bvy986xhapyjlicbi5y&st=xqjvj8ll&dl=1";
     const link = document.createElement("a");
     link.href = fileUrl;
     link.download = "Lumen-v1.0.zip";
@@ -23528,7 +23528,7 @@ function DownloadModpackPage() {
       /* @__PURE__ */ jsxRuntimeExports.jsx(
         "a",
         {
-          href: "https://www.dropbox.com/scl/fi/5fyyc5ut4uki0in5n42de/Lumen-v1.5.zip?rlkey=e7cdjysnz3dpsegctsobbuahu&st=xfy64z7q&dl=1",
+          href: "https://www.dropbox.com/scl/fi/6jxupkt4jl0jxqub2m7gp/Lumen-2.2.zip?rlkey=10afp3bvy986xhapyjlicbi5y&st=xqjvj8ll&dl=1",
           className: "text-primary hover:underline font-medium",
           children: "clique aqui"
         }
